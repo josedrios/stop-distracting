1090f0978f2abb293d743200899ae4ccf247fce1
Jose Rios <joserios0531@gmail.com>
2026-06-26 14:20:24 -0700

    added button control center

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ document.body.appendChild(overlay);
 // Inserted text to overlay
 var overlayText = "STOP ";
 overlay.innerText = overlayText.repeat(1000);
+
+// 2, 5, 20, 60
+var timeouts = document.createElement("div");
+timeouts.classList.add("button-container");
+
+var twoMin = document.createElement("button");
+twoMin.innerText = "2 min";
+twoMin.classList.add("two-min");
+var fiveMin = document.createElement("button");
+fiveMin.innerText = "5 min";
+fiveMin.classList.add("five-min");
+var twentyMin = document.createElement("button");
+twentyMin.innerText = "20 min";
+twentyMin.classList.add("twenty-min");
+var sixtyMin = document.createElement("button");
+sixtyMin.innerText = "60 min";
+sixtyMin.classList.add("sixty-min");
+
+timeouts.appendChild(twoMin);
+timeouts.appendChild(fiveMin);
+timeouts.appendChild(twentyMin);
+timeouts.appendChild(sixtyMin);
+
+document.body.appendChild(timeouts);
