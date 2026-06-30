68f02544456d62915dd5387ef3c92b87bba12131
Jose Rios <joserios0531@gmail.com>
2026-06-30 16:58:41 -0700

    Organized code

--- script.js
+++ script.js
@@ -17,65 +17,70 @@ function checkInput(min, input) {
 
 // Remove previous components if needed
 function removeComponents() {
-  document.querySelectorAll(".sd-overlay").forEach((overlay) => {
-    overlay.remove();
-  });
-  document.querySelectorAll(".control-center").forEach((control) => {
+  document.querySelectorAll(".sd-root").forEach((control) => {
     control.remove();
   });
 }
 
 function implementComponents() {
-  // Creating the overlay
+  removeComponents();
+  // Creating Containers
+  var root = document.createElement("div");
+  root.classList.add("sd-root");
+
   var overlay = document.createElement("div");
   overlay.className = "sd-overlay";
+
+  var controls = document.createElement("div");
+  controls.classList.add("sd-control");
+
+  document.body.appendChild(root);
+  root.appendChild(overlay);
+  root.appendChild(controls);
+
+  // Populating overlay
   var overlayText = "STOP ";
   overlay.innerText = overlayText.repeat(1000);
 
-  // Creating Control Center Components
-  var controls = document.createElement("div");
-  var timeouts = document.createElement("div");
-  var twoMin = document.createElement("button");
-  var fiveMin = document.createElement("button");
-  var twentyMin = document.createElement("button");
-  var sixtyMin = document.createElement("button");
+  // Setting up controls
   var input = document.createElement("input");
+  input.classList.add("sd-control__input");
   input.placeholder = "Type here...";
-  var note = document.createElement("p");
+  controls.appendChild(input);
 
-  // Adding classes/text to those components
-  controls.classList.add("control-center");
-  timeouts.classList.add("button-container");
-  input.classList.add("control-input");
-  note.classList.add("control-center-note");
-  note.innerText =
-    'Type "Bye Bye Brain" in the input, then choose your break length.';
+  var buttons = document.createElement("div");
+  buttons.classList.add("sd-control__buttons");
+  controls.appendChild(buttons);
+
+  var twoMin = document.createElement("button");
   twoMin.innerText = "2 min";
   twoMin.classList.add("two-min");
+  buttons.appendChild(twoMin);
+  twoMin.addEventListener("click", () => checkInput(2, input));
+
+  var fiveMin = document.createElement("button");
   fiveMin.innerText = "5 min";
   fiveMin.classList.add("five-min");
+  buttons.appendChild(fiveMin);
+  fiveMin.addEventListener("click", () => checkInput(5, input));
+
+  var twentyMin = document.createElement("button");
   twentyMin.innerText = "20 min";
   twentyMin.classList.add("twenty-min");
+  buttons.appendChild(twentyMin);
+  twentyMin.addEventListener("click", () => checkInput(20, input));
+
+  var sixtyMin = document.createElement("button");
   sixtyMin.innerText = "60 min";
   sixtyMin.classList.add("sixty-min");
-
-  // Add to DOM
-  controls.appendChild(input);
-  timeouts.appendChild(twoMin);
-  timeouts.appendChild(fiveMin);
-  timeouts.appendChild(twentyMin);
-  timeouts.appendChild(sixtyMin);
-  controls.appendChild(timeouts);
-  controls.appendChild(note);
-  document.body.appendChild(overlay);
-  document.body.appendChild(controls);
-
-  twoMin.addEventListener("click", () => checkInput(2, input));
-  fiveMin.addEventListener("click", () => checkInput(5, input));
-  twentyMin.addEventListener("click", () => checkInput(20, input));
+  buttons.appendChild(sixtyMin);
   sixtyMin.addEventListener("click", () => checkInput(60, input));
+
+  var note = document.createElement("p");
+  note.classList.add("sd-control__note");
+  note.innerText =
+    'Type "Bye Bye Brain" in the input, then choose your break length.';
+  controls.appendChild(note);
 }
 
-console.log("Stop Distracting extension has been deployed");
-removeComponents();
 implementComponents();
